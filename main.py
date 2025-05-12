e832e312ecc77703ae8b5f1fb8848ce4f6fd175d
Andrew Rowley <andrew.rowley@leonardo.com>
2025-05-12 16:52:13 +0100

    database made, add one row and query

--- main.py
+++ main.py
@@ -4,16 +4,17 @@ import pandas as pd
 from datetime import datetime, timezone
 import csv
 import os
+import psycopg2
 
 OPENWEATHER_API_KEY = os.environ.get("OPENWEATHER_API_KEY")
 
 def get_data_from_latlon(lat, lon):
-    '''Make a request to https://home.openweathermap.org/'''
+    '''Make a request to https://home.openweathermap.org/, return the json response'''
     response = requests.get(f"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={OPENWEATHER_API_KEY}")
     return response.json()
 
 def transform_data(data):
-    '''Take a dict of raw weather data and process it.'''
+    '''Take a dict of raw weather data and process it to another dict.'''
     weather = data['weather'][0]
     weather['utctime'] = datetime.now(timezone.utc).strftime('%Y/%m/%d-%H:%M')
     weather['wind_speed'] = data['wind']['speed']
@@ -31,11 +32,60 @@ if __name__ == '__main__':
     LONGITUDE = -2.564519
 
     data = get_data_from_latlon(LATITUDE, LONGITUDE)
-    weather = transform_data(data)
-    with open("out.csv", "w", newline="") as f:
-        w = csv.DictWriter(f, weather.keys())
-        w.writeheader()
-        w.writerow(weather)
+    weather = transform_data(data) # a dict of transformed weather data
+
+    # create postgres cursor
+    conn = psycopg2.connect(
+        dbname="mydatabase",
+        user="myuser",
+        password="mypassword",
+        host="weather_db",
+        port="5432"
+    )
+    cursor = conn.cursor()
+
+    # make table
+    cursor.execute(
+        """
+        CREATE TABLE IF NOT EXISTS weather (
+            description TEXT NOT NULL,
+            utctime TEXT NOT NULL,
+            wind_speed FLOAT,
+            wind_dir INT,
+            humidity INT,
+            feels_like FLOAT,
+            temp_min FLOAT,
+            temp_max FLOAT
+        );
+        """
+    )
+
+    # insert data
+    cursor.execute(
+        """
+        INSERT INTO weather (
+            description,
+            utctime,
+            wind_speed,
+            wind_dir,
+            humidity,
+            feels_like,
+            temp_min,
+            temp_max
+        ) VALUES (%s, %s, %s, %s, %s, %s, %s, %s);
+        """, 
+        tuple(weather.values())
+    )
+
+    # query table for testing
+    res = cursor.execute(
+        """
+        SELECT * FROM weather
+        """
+    )
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
 
 
 
